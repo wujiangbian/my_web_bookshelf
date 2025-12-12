58a82f5223c7e969ee35613c02aea45a2a697441
wujiangbian <3215313197@qq.com>
2025-12-12 22:25:58 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 // vue.config.js
 module.exports = {
-  publicPath: "my_web_bookshelf",
+  publicPath:  process.env.NODE_ENV === 'production'
+      ? '/my_web_bookshelf/'
+      :'/',
   productionSourceMap: false,
   devServer: {
     port: 8081
